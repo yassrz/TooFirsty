2900d0b1a08cb6b091c8dcda6e7be62f19d5a0b5
yassrz <yasirarafatmakak75@gmail.com>
2024-03-30 10:57:15 +0530

    first push

---
+++ test.cpp
@@ -0,0 +1,25 @@
+/*#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b;
+    cout<< "please enter two number: ";
+    cin>>a>>b;
+    cout<<"sum of a and b is "<<a+b<<endl;
+    cout<<"division of a and b is "<<a/b<<endl;
+    return 0;
+}*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+     
+    return 0;
+}
+
+
+
+
+
