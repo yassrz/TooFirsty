9e58db8899ffd15cc220f79632493571ce84ecdf
yassrz <yasirarafatmakak75@gmail.com>
2024-03-30 11:04:05 +0530

    comment out

--- test.cpp
+++ test.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 using namespace std;
 
 int main()
@@ -9,15 +9,15 @@ int main()
     cout<<"sum of a and b is "<<a+b<<endl;
     cout<<"division of a and b is "<<a/b<<endl;
     return 0;
-}*/
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a,b;
-     
-    return 0;
 }
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+     
+//     return 0;
+// }
 
 
 
